8bd7b0807f7f0fb7669c075564627a430e553d52
Donte Henley <donte@crowdfavorite.com>
2023-01-11 14:13:35 -0600

    Set up local recipes

---
+++ resources/views/recipes.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cookbook</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+
+<body>
+    <header>
+        <h1>Cookbook</h1>
+    </header>
+    <article>
+        <h2>
+            <a href="/recipes/recipe-1">Recipe Name</a>
+        </h2>
+        <div>
+            <!-- Need to escape mealtime -->
+            Mealtime: Breakfast, Lunch, Dinner
+        </div>
+        <div>
+            Cuisine: Asian, American
+        </div>
+        <div>
+            Recipe steps:
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus aut nulla quaerat dicta est exercitationem beatae qui, laborum, voluptatum sint architecto quasi sit expedita! Ratione non blanditiis nostrum at sit.
+        </div>
+        <div>
+            <h3>Ingredients</h3>
+            <ul>
+                <li>Ingredient 1</li>
+                <li>Ingredient 2</li>
+                <li>Ingredient 3</li>
+                <li>Ingredient 4</li>
+            </ul>
+        </div>
+        <p>Recipe By: Fitmen Cook</p>
+        <p>Added By Donte</p>
+    </article>
+    <article>
+        <h2>Recipe Name</h2>
+        <div>
+            <!-- Need to escape mealtime -->
+            Mealtime: Breakfast, Lunch, Dinner
+        </div>
+        <div>
+            Cuisine: Asian, American
+        </div>
+        <div>
+            Recipe steps:
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus aut nulla quaerat dicta est exercitationem beatae qui, laborum, voluptatum sint architecto quasi sit expedita! Ratione non blanditiis nostrum at sit.
+        </div>
+        <div>
+            <h3>Ingredients</h3>
+            <ul>
+                <li>Ingredient 1</li>
+                <li>Ingredient 2</li>
+                <li>Ingredient 3</li>
+                <li>Ingredient 4</li>
+            </ul>
+        </div>
+        <p>Recipe By: Fitmen Cook</p>
+        <p>Added By Donte</p>
+    </article>
+    <article>
+        <h2>Recipe Name</h2>
+        <div>
+            <!-- Need to escape mealtime -->
+            Mealtime: Breakfast, Lunch, Dinner
+        </div>
+        <div>
+            Cuisine: Asian, American
+        </div>
+        <div>
+            Recipe steps:
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus aut nulla quaerat dicta est exercitationem beatae qui, laborum, voluptatum sint architecto quasi sit expedita! Ratione non blanditiis nostrum at sit.
+        </div>
+        <div>
+            <h3>Ingredients</h3>
+            <ul>
+                <li>Ingredient 1</li>
+                <li>Ingredient 2</li>
+                <li>Ingredient 3</li>
+                <li>Ingredient 4</li>
+            </ul>
+        </div>
+        <p>Recipe By: Fitmen Cook</p>
+        <p>Added By Donte</p>
+        <p><a href="/">Back to home</a></p>
+    </article>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('recipes');
+});
+
+Route::get('recipes/{recipe}', function ($slug) {
+    $path = __DIR__ . "/../resources/recipes/{$slug}.html";
+
+    if (! file_exists($path)) {
+        abort(404);
+    } else {
+
+        $recipe = file_get_contents($path);
+        return view('recipe', [
+            'recipe' => $recipe
+        ]);
+    }
 });
