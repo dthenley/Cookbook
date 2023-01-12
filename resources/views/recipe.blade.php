881ee9f0713a6bc04dbbf9a4dcbaac29907308fa
Donte Henley <donte@crowdfavorite.com>
2023-01-12 14:24:15 -0600

    Setting up layout files

--- resources/views/recipe.blade.php
+++ resources/views/recipe.blade.php
@@ -1,19 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cookbook</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
+@extends('layout')
 
-<body>
+@section('content')
     <article>
         <h1>
                 {{$recipe->title}}
         </h1>
         {!!$recipe->body!!}
     </article>
-</body>
-</html>
+@endsection
--- resources/views/recipes.blade.php
+++ resources/views/recipes.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cookbook</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
 
-<body>
+@extends('layout')
+
+@section('content')
     <header>
         <h1>Cookbook</h1>
     </header>
@@ -24,5 +17,4 @@
             {!!$recipe->body!!}
         </article>
     @endforeach
-</body>
-</html>
+@endsection
