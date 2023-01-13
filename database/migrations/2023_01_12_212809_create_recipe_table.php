44757900e5daad560ea11a49a393605b134edd03
Donte Henley <donte@crowdfavorite.com>
2023-01-13 10:50:48 -0600

    setting up default seeding for site

--- database/migrations/2023_01_12_212809_create_recipe_table.php
+++ database/migrations/2023_01_12_212809_create_recipe_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('recipes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
             $table->foreignId('category_id');
             $table->string('slug');
             $table->string('title');
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Recipe;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +18,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Recipe::truncate();
+
+        $user = User::factory(10)->create();
+
+        $american = Category::create(
+            [
+                'name' => 'American',
+                'slug' => 'american'
+            ]
+        );
+
+        $asian = Category::create(
+            [
+                'name' => 'Asian',
+                'slug' => 'asian'
+            ]
+        );
+
+        $italian = Category::create(
+            [
+                'name' => 'Italian',
+                'slug' => 'italian'
+            ]
+        );
+
+        Recipe::create(
+            [
+                'user_id' => 1,
+                'category_id' => $asian->id,
+                'title' => 'Chicken Coconut Curry',
+                'slug' => 'chicken-coconut-curry',
+                'excerpt' => 'Easy healthy meal prep option',
+                'body' => 'Create curry with cubed chicken, coconut milk, red curry paste, coconut sugar, veggie broth.',
+            ]
+        );
+
+        Recipe::create(
+            [
+                'user_id' => 1,
+                'category_id' => $american->id,
+                'title' => 'Cheeseburger',
+                'slug' => 'cheeseburger',
+                'excerpt' => 'Nice juicy cheeseburger for all occasians',
+                'body' => '1/4 pound of ground beef, chesse, two slices of bread and pickles and lettuce',
+            ]
+        );
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
