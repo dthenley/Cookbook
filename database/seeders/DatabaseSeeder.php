5c44008d3659731c84031da60561e01b07c81dcb
Donte Henley <donte@crowdfavorite.com>
2023-01-13 11:25:01 -0600

    Seed the database from factory data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,55 +22,7 @@ class DatabaseSeeder extends Seeder
         Category::truncate();
         Recipe::truncate();
 
-        $user = User::factory(10)->create();
+        Recipe::factory(10)->create();
 
-        $american = Category::create(
-            [
-                'name' => 'American',
-                'slug' => 'american'
-            ]
-        );
-
-        $asian = Category::create(
-            [
-                'name' => 'Asian',
-                'slug' => 'asian'
-            ]
-        );
-
-        $italian = Category::create(
-            [
-                'name' => 'Italian',
-                'slug' => 'italian'
-            ]
-        );
-
-        Recipe::create(
-            [
-                'user_id' => 1,
-                'category_id' => $asian->id,
-                'title' => 'Chicken Coconut Curry',
-                'slug' => 'chicken-coconut-curry',
-                'excerpt' => 'Easy healthy meal prep option',
-                'body' => 'Create curry with cubed chicken, coconut milk, red curry paste, coconut sugar, veggie broth.',
-            ]
-        );
-
-        Recipe::create(
-            [
-                'user_id' => 1,
-                'category_id' => $american->id,
-                'title' => 'Cheeseburger',
-                'slug' => 'cheeseburger',
-                'excerpt' => 'Nice juicy cheeseburger for all occasians',
-                'body' => '1/4 pound of ground beef, chesse, two slices of bread and pickles and lettuce',
-            ]
-        );
-
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
