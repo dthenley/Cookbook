a8acfbdd9ca162af95be2c137d330c2c82dae3fe
Donte Henley <donte@crowdfavorite.com>
2023-01-17 08:59:55 -0600

    Css cleanup

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cookbook</title>
     <link rel="stylesheet" href="/app.css">
-    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+
 
 </head>
 
