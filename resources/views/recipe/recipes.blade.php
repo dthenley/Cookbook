f144bef4ead4b127938b3a5d08d2bdcc4a570efa
Donte Henley <donte@crowdfavorite.com>
2023-01-18 17:01:16 -0600

    Adding to do's to complete the recipe index page

--- resources/views/recipe/recipes.blade.php
+++ resources/views/recipe/recipes.blade.php
@@ -1,6 +1,7 @@
 <x-layout>
     <aside class="w-3/12 max-w-xs">
         <h3 class="text-lg lg:text-2xl">Cuisine Type:</h3>
+        {{-- TO DO: Turn this into a dropdown for cuisines --}}
         <ul>
             @foreach($categories as $category)
                 <li>
@@ -8,10 +9,19 @@
                 </li>
             @endforeach
         </ul>
+
+        {{-- TO DO: Add Filtering for what foods to include. Preferably search function where you can bulk add, type select/check then find the next one --}}
+
+        {{-- TO DO: Add range Slider for Calories filtering --}}
+        {{-- TO DO: Add range Slider for Carbs filtering --}}
+        {{-- TO DO: Add range Slider for Proteins filtering --}}
+        {{-- TO DO: Add range Slider for Fat filtering --}}
     </aside>
     <div class="grid sm:grid-cols-2 2xl:grid-cols-3 gap-4">
         <h1 class="col-span-full text-xl lg:text-4xl ">Recipes</h1>
+        {{-- TO DO: Add filters for more accurate search. Make it compound filtering where user can keep adding on filters then have the option to delete each one by one --}}
         @foreach ($recipes as $recipe)
+            {{-- TO DO: extract these into their own blade directive ie. _recipe-card.blade.php --}}
             <article class="grid grid-cols-6 items-start gap-2 rounded-lg bg-slate-200 dark:bg-white text-slate-800">
                 <a href="/recipes/{{$recipe->slug}}" class="col-span-2 p-3">
                     <img src="https://via.placeholder.com/100" width="100" height="100" alt="image of {{$recipe->title}}" class="rounded-full">
