9bc50a4e0d25efe20eb867cb2174baeecef8c98c
Donte Henley <donte@crowdfavorite.com>
2023-01-19 09:56:29 -0600

    Add conditional for post view

--- resources/views/recipe/recipes.blade.php
+++ resources/views/recipe/recipes.blade.php
@@ -1,11 +1,17 @@
 <x-layout>
     @include ('recipe._sidebar-filter')
 
-    <div class="grid sm:grid-cols-2 2xl:grid-cols-3 gap-4">
-        <h1 class="col-span-full text-xl lg:text-4xl ">Recipes</h1>
+    <div>
+        <h1 class="text-xl lg:text-4xl mb-3">Recipes</h1>
         {{-- TO DO: Add filters for more accurate search. Make it compound filtering where user can keep adding on filters then have the option to delete each one by one --}}
-        @foreach ($recipes as $recipe)
-            <x-recipe-card :recipe="$recipe" />
-        @endforeach
+        @if ($recipes->count())
+            <div class="grid sm:grid-cols-2 2xl:grid-cols-3 gap-4">
+                @foreach ($recipes as $recipe)
+                    <x-recipe-card :recipe="$recipe" />
+                @endforeach
+            </div>
+        @else
+            <h3>No Recipes Found</h3>
+        @endif
     </div>
 </x-layout>
